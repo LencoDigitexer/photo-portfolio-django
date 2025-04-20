ff1bc5ffe91d49db501411d7e5c1e2e61ca0734a
Илья Евсеенко <addminuser258@gmail.com>
2025-04-20 19:43:48 +0300

    first commit

---
+++ photo_portfolio/photos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Photo
+from .serializers import PhotoSerializer
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all().order_by('-created_at')
+    serializer_class = PhotoSerializer
